7af4b8a7a22c4f371e7cbf86b33339621b331099
Ariiadnee-06 <ariadne.castaneda844@pascualbravo.edu.co>
2025-04-04 09:13:35 -0500

    Mas cambios

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -13,3 +13,30 @@ if (navClose) {
         navMenu.classList.remove('show_menu');
     });
 };
+
+/*-----------Formulario----------------*/
+const openFormBtn = document.getElementById('openNew')
+const formModal = document.getElementById('form_modal');
+const closeModal = document.getElementById('close_modal');
+
+openFormBtn.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    formModal.classList.remove('hidden');
+});
+
+closeModal.addEventListener('click', () => {
+    formModal.classList.add('hidden');
+});
+
+const openOldFormBtn = document.getElementById('openOld')
+const formOldModal = document.getElementById('form_modalOld');
+const closeOldModal = document.getElementById('close_modalOld');
+
+openOldFormBtn.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    formOldModal.classList.remove('hidden');
+});
+
+closeOldModal.addEventListener('click', () => {
+    formOldModal.classList.add('hidden');
+});
