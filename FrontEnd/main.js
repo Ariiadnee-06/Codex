916cac0c81ad1123bfeeccff933e355fc489560a
Ariiadnee-06 <ariadne.castaneda844@pascualbravo.edu.co>
2025-04-03 21:27:20 -0500

    Cambios de JavaScript

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -2,25 +2,14 @@ const navMenu = document.getElementById('nav_menu'),
       navToggle = document.getElementById('nav_toggle'),
       navClose = document.getElementById('nav_close');
 
-// ABRIR MENÚ
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show_menu');
     });
 }
 
-// CERRAR MENÚ
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show_menu');
     });
-}
-
-// CERRAR MENÚ AL HACER CLIC EN UN ENLACE
-const navLinks = document.querySelectorAll('.nav_list a');
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navMenu.classList.remove('show_menu');
-    });
-});
+};
