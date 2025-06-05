625d36606ca41f1f0d406b9b3c9697cec77d891a
Ariiadnee-06 <ariadne.castaneda844@pascualbravo.edu.co>
2025-06-05 09:58:01 -0500

    Agregue panel de administracion

---
+++ codex/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using codex.Data;
+using codex.Models;
+
+namespace codex.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Admin()
+        {
+            var computadoras = _context.Computadoras.ToList();
+            return View(computadoras);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Computadora computadora)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Computadoras.Add(computadora);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Crud");
+        }
+
+        [HttpPost]
+        public IActionResult Update(Computadora computadora)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Computadoras.Update(computadora);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Crud");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var computadora = _context.Computadoras.Find(id);
+            if (computadora != null)
+            {
+                _context.Computadoras.Remove(computadora);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Crud");
+        }
+    }
+}
