754160beec06ba9513b0d2aa0ceeda3af29680c0
JuanPVelasquezR <velasquezramirezjuanpablo@gmail.com>
2025-06-06 22:42:26 -0500

    Mejora vista Catálogo

--- codex/obj/Debug/net8.0/codex.AssemblyInfo.cs
+++ codex/obj/Debug/net8.0/codex.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04c78c7981d21b4fb683a9d14fd4ddbf2f08880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f72d6d31a8d2368eb8c75e2bbece0252a25e84a")]
 [assembly: System.Reflection.AssemblyProductAttribute("codex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
